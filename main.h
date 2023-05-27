8fe640aded47abc8378829fee959217f57892fdb
NanaAba08 <williamsnanaaba@gmail.com>
2023-05-27 13:16:21 -0700

    update

--- main.h
+++ main.h
@@ -5,7 +5,35 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * struct flags - struct that has  flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus - flag for '+' char
+ * @space - flag for '' char
+ * @hash - flag for '#' char
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int harsh;
+} flags_tm;
 
+
+/**
+  * struct print_handle - Entry point
+  * Description - struct to aid in selecting the right function according to the
+  * format specifier passed to the _printf function
+  * @c: format specifier
+  * @f: pointer to the printing function
+  */
+typedef struct print_handle
+{
+	char c;
+	int (*f)(va_list ap, flags_tm *f);
+} ph;
+
+/* print output according to format */
 int _printf(const char *format, ...);
 
 
