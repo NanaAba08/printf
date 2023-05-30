cca86b7f95c46ea92a13da388d986403684c2a91
NanaAba08 <williamsnanaaba@gmail.com>
2023-05-30 02:56:33 -0700

    main.h update

--- main.h
+++ main.h
@@ -8,6 +8,20 @@
 #include <stddef.h>
 #include <unistd.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* For Flags */
+#define F_MINUS 1 /* flag representing '-' flag */
+#define F_PLUS 2 /* flag representing '+' flag */
+#define F_ZERO 4 /* flag representing '0' flag */
+#define F_HARSH 8 /* flag representing '#' flag */
+#define F_SPACE 16 /*flag representing ' ' flag */
+
+/* For sizes */
+#define S_LONG 1
+#define S_SHORT 2
+
 int _printf(const char *format, ...);
 int _putchar(int chr);
 int print_string(const char *str);
